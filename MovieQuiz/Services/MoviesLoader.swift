eee1fcd5b97f74cfc50aaa7bae0a5ed3e8ad7b56
YakovNem <113629638+YakovNem@users.noreply.github.com>
2022-11-19 15:52:09 +0300

    Sprint_06

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,45 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Yakov Nemychenkov on 16.11.2022.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_s5z5b7qx") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+
+
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            
+            case .failure(let error):
+                handler(.failure(error))
+            case .success(let data):
+                do {
+                    let movies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(movies))
+                } catch let error {
+                    handler(.failure(error))
+                }
+            }
+        }
+    
+    }
+}
+
